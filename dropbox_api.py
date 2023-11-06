c47df377637df2fdc1b730d8ceeab48f10aa402f
Tim-a-tree <jeonghyunheo619@gmail.com>
2023-11-06 20:55:44 +0900

    ADDED: now uploads to the account

--- dropbox_api.py
+++ dropbox_api.py
@@ -1,38 +1,40 @@
 import dropbox
+import contextlib
 import pathlib
 import dotenv
 import os
 
 
-def connect_to_dropbox(access_token): 
-    try: 
-        dbx = dropbox.Dropbox(access_token) 
-        print('Connected to Dropbox successfully')
+def connect_to_dropbox(access_token):
+    try:
+        dbx = dropbox.Dropbox(access_token)
+        print("Connected to Dropbox successfully")
         return dbx
-    except Exception as e: 
-        print(str(e)) 
-    
+    except Exception as e:
+        print(str(e))
+
     return None
 
-def list_files_in_folder(dbx): 
-    # here dbx is an object which is obtained 
-    # by connecting to dropbox via token 
-    try: 
+
+def list_files_in_folder(dbx):
+    # here dbx is an object which is obtained
+    # by connecting to dropbox via token
+    try:
         folder_path = "/testing"
-  
-        # dbx object contains all functions that  
-        # are required to perform actions with dropbox 
-        files = dbx.files_list_folder(folder_path).entries 
-        print("------------Listing Files in Folder------------ ") 
+
+        # dbx object contains all functions that
+        # are required to perform actions with dropbox
+        files = dbx.files_list_folder(folder_path).entries
+        print("------------Listing Files in Folder------------ ")
+
+        for file in files:
+            # listing
+            print(file.name)
+
+    except Exception as e:
+        print(str(e))
 
 
-        for file in files: 
-            # listing 
-            print(file.name) 
-
-    except Exception as e: 
-        print(str(e)) 
-  
 def list_folder(dbx):
     try:
         folders = dbx.list_folder().entries
@@ -41,23 +43,39 @@ def list_folder(dbx):
         for folder in folders:
             print(folder.name)
 
-    except Exception as e: 
-        print(str(e)) 
-
-def upload(dbx, local_file_dir):
-    dropbox_path = "" # TODO: Will be updated
-
-    try:
-        for local_file in local_file_list:
-            if file.endswith(".arw") or file.endswith(".ARW"):
-                local_file_path = pathlib.Path(local_file)
-
-    
-                with local_file_path.open("rb") as f:
-                    dbx.files_upload(f.read(), dropbox_path)
-
-
     except Exception as e:
-        print("Error on uploading file", local_file)
+        print(str(e))
 
 
+def upload_files(dbx, local_path):
+    dropbox_path = "/testing/"  # TODO: Will be updated
+
+    file_list = os.listdir(local_path)
+
+    for file in file_list:
+        if file.endswith(".arw") or file.endswith(".ARW") and not file.startswith("."):
+            fullname = os.path.join(local_path, file)
+            print("DEBUG - found file ", fullname)
+
+            with open(fullname, "rb") as f:
+                print("uploading ", fullname, "with", dropbox_path + file)
+                try:
+                    dbx.files_upload(
+                        f.read(),
+                        dropbox_path + file,
+                        mode=dropbox.files.WriteMode.overwrite,
+                    )
+
+                except dropbox.exceptions.ApiError as e:
+                    print("Error on uploading file", file)
+
+
+@contextlib.contextmanager
+def stopwatch(message):
+    """Context manager to print how long a block of code took."""
+    t0 = time.time()
+    try:
+        yield
+    finally:
+        t1 = time.time()
+        print("Total elapsed time for %s: %.3f" % (message, t1 - t0))
--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 #     1. convert .arw file to .jpg
 #     2. multi-threading of the program according to the number of .arw files
 
-
+from dotenv import load_dotenv
 import os
 import shutil
 from datetime import datetime
@@ -16,8 +16,9 @@ import psutil
 import time
 from sys import platform
 import user
-import dropbox_api
+import dropbox_api as dba
 import dropbox
+
 
 def get_current_status():
     print("Getting current directory: ")
@@ -48,7 +49,7 @@ def auto_detect_sd_card(drives):
                 return "/Volumes/" + result[0]
             else:
                 if len(arr) < len(drives):
-                    print("Updating connected devices: ", arr) # DEBUG
+                    print("Updating connected devices: ", arr)  # DEBUG
                 drives = arr
         elif platform == "win32":
             arr = [partition.device for partition in psutil.disk_partitions()]
@@ -78,8 +79,6 @@ def create_shutterpresso_dir(id):
 
     shutterpresso_dir = shutterpresso_dir + "_" + str(date) + "_" + str(id)
 
-
-
     # if the directory already exists, create a new one with a number at the end
     if os.path.exists(shutterpresso_dir):
         count = 1
@@ -94,21 +93,23 @@ def create_shutterpresso_dir(id):
     print("Directory created : ", shutterpresso_dir)
     return shutterpresso_dir
 
+
 # WARNING: detects the id only using first file in the list
 def find_id(user_list, file):
-# detecting filename matches with the dictionary
+    # detecting filename matches with the dictionary
     print("Received", user_list, file)
     for user in user_list.keys():
-        print("Checking ", user) # DEBUG
+        print("Checking ", user)  # DEBUG
         values = user_list.get(user)
         for value in values:
             if value.lower() in file.lower():
-                print("Matching: ", value.lower(), user, file.lower()) # DEBUG
+                print("Matching: ", value.lower(), user, file.lower())  # DEBUG
                 id = str(user)
                 print("The SD card belongs to ", id)
                 return id
 
-#copy files to the shutterpresso folder
+
+# copy files to the shutterpresso folder
 def copy_files_to_shutterpresso_dir(sd_card_dir, user_list):
     # copy all .arw files in DCIM folder
     DCIM_dir = os.path.join(str(sd_card_dir), "DCIM")
@@ -117,7 +118,6 @@ def copy_files_to_shutterpresso_dir(sd_card_dir, user_list):
         print("ERROR : DCIM_dir does not exist")
         return
 
-    
     print("Start copying files to shutterpresso directory")
     files_in_DCIM = os.listdir(DCIM_dir)
 
@@ -130,8 +130,11 @@ def copy_files_to_shutterpresso_dir(sd_card_dir, user_list):
             shutil.copy(os.path.join(DCIM_dir, file), shutterpresso_dir)
 
     return shutterpresso_dir
-    
+
+
 def main():
+    load_dotenv()
+
     drives = get_current_status()
     user_list = user.read_users()
 
@@ -146,24 +149,23 @@ def main():
     sd_card_dir = auto_detect_sd_card(drives)
 
     # shutterpresso_dir = create_shutterpresso_dir()
-    
+
     shutterpresso_dir = copy_files_to_shutterpresso_dir(sd_card_dir, user_list)
 
     # Dropbox authentication
-    access_token = os.environ['ACCESS_TOKEN']
-    dbx = dropbox_api.connect_to_dropbox(access_token)
+    access_token = os.environ["ACCESS_TOKEN"]
+    dbx = dba.connect_to_dropbox(access_token)
 
     # create a folder in dropbox
-    try:
-        dbx.files_create_folder_v2(dbx, shutterpresso_dir)
-        print("Folder created on Dropbox with name: ", shutterpresso_dir)
+    # try:
+    #     dbx.files_create_folder_v2(dbx, shutterpresso_dir)
+    #     print("Folder created on Dropbox with name: ", shutterpresso_dir)
 
-    except dropbox.exceptions.ApiError as e:
-        print("Error creating folder: ", e)
-
+    # except dropbox.exceptions.ApiError as e:
+    #     print("Error creating folder: ", e)
 
     # uploading to dropbox
-    upload(dbx, shutterpresso_dir)
+    dba.upload_files(dbx, shutterpresso_dir)
     # go back to while loop
     main()
 
